4af885a5013c2d6315f9dd42effb38bab5480f1b
Sourav Barua <sourav.shr6@gmail.com>
2021-07-13 23:06:30 -0230

    Selected trip bar plot completed

--- frontend/src/components/AttributeContribution/AttributeContribution.js
+++ frontend/src/components/AttributeContribution/AttributeContribution.js
@@ -41,14 +41,84 @@ export const AttributeContribution = ({selectedAttr,selectedTrip,scores}) => {
         .range([height, 0]);
     svg.append("g")
           .call(d3.axisLeft(yScale));
+    svg.append("circle").attr("cx",width-200).attr("cy",40).attr("r", 6).style("fill", "#3b7406")
+    svg.append("circle").attr("cx",width-200).attr("cy",60).attr("r", 6).style("fill", "#ee4266")
+    svg.append("text").attr("x", width-190).attr("y", 45).text("Mean Trip").style("font-size", "15px").attr("alignment-baseline","middle")
+    svg.append("text").attr("x", width-190).attr("y", 65).text("Selected Trip").style("font-size", "15px").attr("alignment-baseline","middle")
     
   }, [])
 
   // This hook will be called when a new trip is selected on the scoretable
   useEffect(() => {
     //console.lo g(selectedTrip);
-    setTripID(selectedTrip.id);
-  }, [selectedTrip])
+    let tripId, selectedTripScore;
+    if(selectedTrip != null){
+      setTripID(selectedTrip.tripId);
+      //console.log(selectedTrip.tripId);
+      // Prepping the selected trip data
+      tripId = selectedTrip.tripId;
+      selectedTripScore = scores.find(x => x[0] == tripId);
+      selectedTripScore = selectedTripScore.map(x => x.attrContribution);
+      selectedTripScore.splice(0, 2);
+      let selectedTripAttrContrib =new Array(selectedAttr.length).fill(0);
+      selectedTripAttrContrib.forEach((element,index, arr) => {
+          arr[index] = selectedTripScore.reduce((accum, curr) => accum + curr[index],0)
+      });
+      selectedTripAttrContrib = selectedTripAttrContrib.map(x=>x/selectedTripScore.length);
+      console.log(selectedTripAttrContrib);
+      // creating the viz
+      xRange= range(20, width, width/selectedAttr.length);
+      xScale = d3.scaleOrdinal().domain(selectedAttr).range(xRange);
+      yScale = d3.scaleLinear()
+        .domain([0, 100])
+        .range([height, 0]);
+      svg = d3.select("#attrContribWrapper #attrContribSVG g");
+
+      if(!svg.select(".selectedTripBars").empty()){
+        let bars = svg.selectAll('.selectedTripBars').data(selectedTripAttrContrib);
+        bars.enter()
+        .append("rect")
+          .attr("class","selectedTripBars")
+          .attr("x", function(d, i) { return xScale(selectedAttr[i])-15; })
+          .attr("y", function(d) { return yScale(d); })
+          .attr("width", 30)
+          .transition()
+          .duration(1500)
+          .attr("height", function(d) { return height - yScale(d); })
+          .attr("fill", "#ee4266")
+          .style("opacity",0.7);
+        
+        bars
+        .transition()
+        .duration(1500)
+          .attr("x", function(d, i) { return xScale(selectedAttr[i])-15; })
+          .attr("y", function(d) { return yScale(d); })
+          .attr("width", 30)
+          .attr("height", function(d) { return height - yScale(d); })
+          .attr("fill", "#ee4266")
+          .style("opacity",0.7);
+        
+        bars.exit().remove();
+        
+      }
+      else{
+        svg.selectAll("selectedTripBar")
+        .data(selectedTripAttrContrib)
+        .enter()
+        .append("rect")
+          .attr("class","selectedTripBars")
+          .transition()
+          .duration(1500)
+            .attr("x", function(d, i) { return xScale(selectedAttr[i])-15; })
+            .attr("y", function(d) { return yScale(d); })
+            .attr("width", 30)
+            .attr("height", function(d) { return height - yScale(d); })
+            .attr("fill", "#ee4266")
+            .style("opacity",0.7)
+      }
+      
+    }
+  }, [selectedTrip, scores])
 
   // This hook will be called when the selected attributes will change
   useEffect(() => {
@@ -59,11 +129,11 @@ export const AttributeContribution = ({selectedAttr,selectedTrip,scores}) => {
     xAxis = d3.select("g#xAxis");
     xAxis.transition().duration(2000).call(d3.axisBottom(xScale))
   }, [selectedAttr]);
-
+  // This hook will be called when the calculated score changess
   useEffect(() => {
     let meanTripScore;
     if (scores!= null){
-      // Creating the mean trajectory attr contribution viz
+      // Prepping the mean trajectory attr contribution data
       meanTripScore = scores.find(x => x[0] == 162); // finding the mean trip in scores
       meanTripScore = meanTripScore.map(x=>x.attrContribution);
       meanTripScore.splice(0,2);
@@ -72,7 +142,7 @@ export const AttributeContribution = ({selectedAttr,selectedTrip,scores}) => {
           arr[index] = meanTripScore.reduce((accum, curr) => accum + curr[index],0)
       });
       meanTripAttrContrib = meanTripAttrContrib.map(x=>x/meanTripScore.length);
-      
+      // viz setup for the mean attr contrib
       xRange= range(20, width, width/selectedAttr.length);
       xScale = d3.scaleOrdinal().domain(selectedAttr).range(xRange);
       yScale = d3.scaleLinear()
@@ -87,7 +157,7 @@ export const AttributeContribution = ({selectedAttr,selectedTrip,scores}) => {
           .duration(1500)
           .attr("id", "meanLine")
           .attr("fill", "none")
-          .attr("stroke", "#69b3a2")
+          .attr("stroke", "#0ead69")
           .attr("stroke-width", 2.5)
           .attr("d", d3.line()
             .x(function(d, i) { return xScale(selectedAttr[i]) })
@@ -99,7 +169,7 @@ export const AttributeContribution = ({selectedAttr,selectedTrip,scores}) => {
           .datum(meanTripAttrContrib)
           .attr("id", "meanLine")
           .attr("fill", "none")
-          .attr("stroke", "#69b3a2")
+          .attr("stroke", "#0ead69")
           .attr("stroke-width", 2.5)
           .attr("d", d3.line()
             .x(function(d, i) { return xScale(selectedAttr[i]) })
@@ -111,12 +181,12 @@ export const AttributeContribution = ({selectedAttr,selectedTrip,scores}) => {
         let dots = svg.selectAll('.dots').data(meanTripAttrContrib);
         dots.enter().append('circle')
           .transition()
-          .duration(1500)
+          .duration(1000)
           .attr("cx", function(d, i) { return xScale(selectedAttr[i]) })
           .attr("cy", function(d) { return yScale(d) })
           .attr("r", 7)
           .attr("class","dots")
-          .style("fill", "green");
+          .style("fill", "#3b7406");
 
         dots  
         .transition()
@@ -124,12 +194,12 @@ export const AttributeContribution = ({selectedAttr,selectedTrip,scores}) => {
           .attr("cx", function(d, i) { return xScale(selectedAttr[i]) })
           .attr("cy", function(d) { return yScale(d) })
           .attr("r", 7)
-          .style("fill", "green");
+          .style("fill", "#3b7406");
         dots.exit().remove();
       }
       else{
         svg
-        .selectAll('circle')
+        .selectAll('circle .dots')
         .data(meanTripAttrContrib)
         .enter()
         .append('circle')
@@ -137,7 +207,7 @@ export const AttributeContribution = ({selectedAttr,selectedTrip,scores}) => {
           .attr("cx", function(d, i) { return xScale(selectedAttr[i]) })
           .attr("cy", function(d) { return yScale(d) })
           .attr("r", 7)
-          .style("fill", "green")
+          .style("fill", "#3b7406")
         }
      }
   }, [scores])
